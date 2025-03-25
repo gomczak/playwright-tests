909eede586f7e8fc5242f9c9a926f2dd3c611241
Małgorzata Tomczak <tomczakmalgorzata2@gmail.com>
2025-03-25 20:02:45 +0100

    sharding tests

--- tests/e2e/signup.spec.ts
+++ tests/e2e/signup.spec.ts
@@ -4,3 +4,8 @@ test('sign up page', async ({ page }) => {
     await page.goto('http://localhost:3000/signup');
     await expect(page.locator('h2')).toHaveText('Sign up');
 });
+
+test('sign up page 2', async ({ page }) => {
+    await page.goto('http://localhost:3000/signup');
+    await expect(page.locator('h2')).toHaveText('Sign up');
+});
