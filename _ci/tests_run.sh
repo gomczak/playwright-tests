c5571fa6a0f7b93a3c51fb085d175e0e0a66e532
Małgorzata Tomczak <tomczakmalgorzata2@gmail.com>
2025-03-27 21:26:50 +0100

    remove run sh

--- _ci/tests_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "is running test_run.sh"
-pnpm exec playwright test \
-${selected_options[@]} \
-${{ github.event.inputs.files }} \
---shard=${{ matrix.shardIndex }}/${{ matrix.shardTotal }} \
---reporter allure-playwright
