0df63d7c29def849dcc17c9e2e5ff44e49eec7d9
Małgorzata Tomczak <tomczakmalgorzata2@gmail.com>
2025-03-29 20:24:38 +0100

    fix permissions

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title FAILED', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Tlaywright/);
 });
 
 test('get started link', async ({ page }) => {
