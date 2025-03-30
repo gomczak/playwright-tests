4777fdf7b90ba89025c7cdf34830fee24a12de37
Małgorzata Tomczak <tomczakmalgorzata2@gmail.com>
2025-03-30 19:02:10 +0200

    allure epic

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -5,6 +5,8 @@ import * as allure from "allure-js-commons";
 test.describe('Login tests', {
     tag: '@logintag',
 }, () => {
+    allure.epic("TEST Web interface");
+
     test('log in page', {
         annotation: {
             type: 'issue',
@@ -20,7 +22,8 @@ test.describe('Login tests', {
         await allure.epic("Web interface");
         await allure.feature("Essential features");
         await allure.story("Authentication");
-        await allure.issue("123")
+        await allure.issue("123");
+        await allure.link("ABC-123", "Related Jira issue", "jira");
 
         await page.goto('http://localhost:3000/login');
         await expect(page.locator('h2')).toHaveText('Log in');
